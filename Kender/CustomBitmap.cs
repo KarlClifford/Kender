43563c8478eb004bc79c077de04195bc4fcf78ae
KarlClifford <65084552+KarlClifford@users.noreply.github.com>
2023-02-10 13:25:05 +0000

    Add project files.

---
+++ Kender/CustomBitmap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Image = System.Drawing.Image;
+
+namespace Kender
+{
+
+    /// <summary>
+    /// This class represents a simple bitmap and makes use of
+    /// the customColour class <see cref="CustomColour"/>.
+    /// </summary>
+    public class CustomBitmap
+    {
+        private readonly int width;
+        private readonly int height;
+        private readonly byte[] imageBytes;
+
+        public CustomBitmap(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            imageBytes = new byte[width * height * 4];
+        }
+
+        public void SetPixel(int x, int y, CustomColour color)
+        {
+            int offset = ((height - y - 1) * width + x) * 4;
+            imageBytes[offset + 0] = color.redColour;
+            imageBytes[offset + 1] = color.greenColour;
+            imageBytes[offset + 2] = color.blueColour;
+        }
+
+        public byte[] GetBitmapBytes()
+        {
+            const int imageHeaderSize = 54;
+            byte[] bmpBytes = new byte[imageBytes.Length + imageHeaderSize];
+            bmpBytes[0] = (byte)'B';
+            bmpBytes[1] = (byte)'M';
+            bmpBytes[14] = 40;
+            Array.Copy(BitConverter.GetBytes(bmpBytes.Length), 0, bmpBytes, 2, 4);
+            Array.Copy(BitConverter.GetBytes(imageHeaderSize), 0, bmpBytes, 10, 4);
+            Array.Copy(BitConverter.GetBytes(width), 0, bmpBytes, 18, 4);
+            Array.Copy(BitConverter.GetBytes(height), 0, bmpBytes, 22, 4);
+            Array.Copy(BitConverter.GetBytes(32), 0, bmpBytes, 28, 2);
+            Array.Copy(BitConverter.GetBytes(imageBytes.Length), 0, bmpBytes, 34, 4);
+            Array.Copy(imageBytes, 0, bmpBytes, imageHeaderSize, imageBytes.Length);
+            return bmpBytes;
+        }
+
+        public void Save(string filename)
+        {
+            byte[] bytes = GetBitmapBytes();
+            File.WriteAllBytes(filename, bytes);
+        }
+
+        public void saveAsJPEG(string filename)
+        {
+            byte[] bytes = GetBitmapBytes();
+            using MemoryStream ms = new(bytes);
+            using Image img = Bitmap.FromStream(ms);
+            img.Save(filename);
+        }
+    }
+}
---
+++ Kender/CustomColour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Kender
+
+{
+    /// <summary>
+    /// This class represents a simple 8-bit colour to be used
+    /// by CustomBitmap <see cref="CustomBitmap"/>.
+    ///
+    /// </summary>
+    public struct CustomColour
+    {
+        /// <summary>
+        /// Stores the 8-bit red value.
+        /// </summary>
+        public readonly byte redColour;
+        /// <summary>
+        /// Stores the 8-bit green value.
+        /// </summary>
+        public readonly byte greenColour;
+        /// <summary>
+        /// Store the 8-bit blue value.
+        /// </summary>
+        public readonly byte blueColour;
+
+        public CustomColour(byte r, byte g, byte b)
+        {
+            (redColour, greenColour, blueColour) = (r, g, b);
+        }
+
+        //public static CustomColour Random(Random rand)
+        //{
+        //    byte r = (byte)rand.Next(256);
+        //    byte g = (byte)rand.Next(256);
+        //    byte b = (byte)rand.Next(256);
+        //    return new CustomColour(r, g, b);
+        //}
+
+        //public static CustomColour Gray(byte value)
+        //{
+        //    return new CustomColour(value, value, value);
+        //}
+    }
+}
