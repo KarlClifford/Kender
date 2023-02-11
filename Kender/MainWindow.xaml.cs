928780fa1bbb4f1ab6610971ad107458358f5a81
KarlClifford <65084552+KarlClifford@users.noreply.github.com>
2023-02-11 16:49:02 +0000

    Setup image render view

--- Kender/MainWindow.xaml.cs
+++ Kender/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -34,10 +36,47 @@ namespace Kender
             this.InitializeComponent();
         }
 
+        public void render(CustomBitmap bitmap) 
+        {
+
+            //get bitmap size so we can iterate and through every pixel
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            //Save slider values
+            double red = redSlider.Value;
+            double green = greenSlider.Value;
+            double blue = blueSlider.Value;
+
+            //Genereate bitmap
+            for(int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    byte r = (byte)red;
+                    byte g = (byte)green;
+                    byte b = (byte)blue;
+                    CustomColour color = new(r, g, b);
+                    bitmap.setPixel(x, y, color);
+                }
+            }
+
+            //Save the generated bitmap
+            bitmap.Save(@"C:\Users\khscl\Downloads\render.bmp");
+
+            //Update the UI to display the new bitmap
+            //renderImage.Source = new BitmapImage(new Uri("render.bmp"), UriKind.Relative);
+            BitmapImage bitmapImage= new BitmapImage();
+            bitmapImage.UriSource = new Uri(@"C:\Users\khscl\Downloads\render.bmp", UriKind.Relative);
+            renderImage.Source = null;
+            renderImage.Source = bitmapImage;
+        }
+
         private void xAxis_SliderChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             //TODO: Implement slider
             Debug.WriteLine($"X Moved: {e.NewValue}");
+
         }
 
         private void yAxis_SliderChanged(object sender, RangeBaseValueChangedEventArgs e)
@@ -56,18 +95,21 @@ namespace Kender
         {
             //TODO: Implement slider
             Debug.WriteLine($"red Moved: {e.NewValue}");
+            render(new CustomBitmap(640, 640));
         }
 
         private void green_SliderChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             //TODO: Implement slider
             Debug.WriteLine($"green Moved: {e.NewValue}");
+            render(new CustomBitmap(640, 640));
         }
 
         private void blue_SliderChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             //TODO: Implement slider
             Debug.WriteLine($"blue Moved: {e.NewValue}");
+            render(new CustomBitmap(640, 640));
         }
 
 
